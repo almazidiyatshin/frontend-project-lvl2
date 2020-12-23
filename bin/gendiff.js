ed157e03b901ad88ca22f62c09f44db782d6b3fd
Almaz Idiyatshin <dev@tshin.ru>
2020-12-23 10:36:25 +0300

    Display help (fix bugs)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,5 @@
-import commander from 'commander';
-
-const { program } = commander;
+#!/usr/bin/env node
+import program from 'commander';
 
 program.version('1.0.0');
 program.description('Compares two configuration files and shows a difference.');
