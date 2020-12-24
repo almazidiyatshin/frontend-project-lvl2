dbcbad1f036a8827855606a7c0d5c07b85c9ca94
Almaz Idiyatshin <dev@tshin.ru>
2020-12-24 09:29:03 +0300

    Add format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import program from 'commander';
 
-program.version('1.0.0');
-program.description('Compares two configuration files and shows a difference.');
-program.parse(process.argv);
+program
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .parse(process.argv);
