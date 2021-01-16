c44feed9243f2fd5e4762377754402e2335a4fd9
Almaz Idiyatshin <dev@tshin.ru>
2021-01-16 13:01:27 +0300

    Update

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
 import parseFile from './parsers.js';
+import render from './stylish.js';
 
-const checkKey = (key, obj1, obj2) => {
+const checkKey = (key, obj1, obj2, depth) => {
   if (key in obj1 && key in obj2) {
-    if (obj1[key] === obj2[key]) {
-      return `  ${key}: ${obj1[key]}`;
+    if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+      const newJointKeys = Object.keys({ ...obj1[key], ...obj2[key] }).sort();
+      const indentForOperator = 2;
+      const children = newJointKeys
+        .map((newKey) => checkKey(newKey, obj1[key], obj2[key], depth + indentForOperator));
+      return {
+        key, status: 'unchanged', children, depth,
+      };
     }
-    return `- ${key}: ${obj1[key]}\n + ${key}: ${obj2[key]}`;
+    if (obj1[key] === obj2[key]) {
+      return {
+        key, value: obj1[key], status: 'unchanged', depth,
+      };
+    }
+    return {
+      key, value: obj2[key], oldValue: obj1[key], status: 'changed', depth,
+    };
   }
   if (key in obj1 && !(key in obj2)) {
-    return `- ${key}: ${obj1[key]}`;
+    return {
+      key, value: obj1[key], status: 'deleted', depth,
+    };
   }
-  return `+ ${key}: ${obj2[key]}`;
+  return {
+    key, value: obj2[key], status: 'added', depth,
+  };
+};
+
+const generateTree = (obj1, obj2) => {
+  const startDepth = 4;
+  const jointKeys = Object.keys({ ...obj1, ...obj2 }).sort();
+  return jointKeys.flatMap((key) => checkKey(key, obj1, obj2, startDepth));
 };
 
 const genDiff = (filepath1, filepath2) => {
   const file1 = parseFile(filepath1);
   const file2 = parseFile(filepath2);
-  const keys = Object.keys({ ...file1, ...file2 }).sort();
-  const arrWithDiffs = keys.map((key) => checkKey(key, file1, file2));
-  const result = `{\n ${arrWithDiffs.join('\n ')}\n}`;
+  const tree = generateTree(file1, file2);
+  const result = render(tree);
+  console.log(tree);
   console.log(result);
   return result;
 };
