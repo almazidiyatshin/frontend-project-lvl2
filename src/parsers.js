c914d81fa755efb33880176f649beb8b21b0fea5
Almaz Idiyatshin <dev@tshin.ru>
2021-01-31 13:33:33 +0300

    Update parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -2,21 +2,15 @@ import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 
-const normalizeFilepath = (filepath) => {
-  if (filepath[0] === '/') {
-    return filepath;
-  }
-  if (filepath.slice(0, 2) === '..') {
-    return path.resolve(`${process.cwd()}${filepath.slice(2)}`);
-  }
-  return path.resolve(`${process.cwd()}${filepath.slice(1)}`);
+const parsers = {
+  json: (file) => JSON.parse(file),
+  yml: (file) => yaml.safeLoad(file),
 };
 
 const parseFile = (filepath) => {
-  if (path.extname(filepath) === '.yml') {
-    return yaml.safeLoad(fs.readFileSync(normalizeFilepath(filepath)), 'utf-8');
-  }
-  return JSON.parse(fs.readFileSync(normalizeFilepath(filepath)), 'utf-8');
+  const fileType = path.extname(filepath).slice(1);
+  const fileData = fs.readFileSync(filepath, 'utf-8');
+  return parsers[fileType](fileData);
 };
 
 export default parseFile;
